75a276a1f3fbdd17c9aeacc46a336fb549658323
HuaHua-Technophile <3118590779@qq.com>
2024-05-23 17:34:47 +0800

    pnpm type/node bootstrap sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path"; // 设置路径别名
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  // 设置路径别名
+  resolve: {
+    // 设置文件./src路径为 @
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, "./src"),
+      },
+    ],
+  },
+});
